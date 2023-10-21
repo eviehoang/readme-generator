5cb307e3f21413c6224fe33eb11ad4e43f957ce7
Evie Hoang <evie.h0325@gmail.com>
2023-10-20 20:49:57 -0700

    Added questions to ask and console log to check.

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,64 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const markdown = require("./utils/generateMarkdown")
 
 // TODO: Create an array of questions for user input
-const questions = [];
+inquirer
+    .prompt([
+        {
+            type: "input",
+            name: "title",
+            message: "What is the name of your project?"
+        },
+        {
+            type: "input",
+            name: "description",
+            message: "Write a brief description about your project."
+        },
+        {
+            type: "input",
+            name: "install",
+            message: "What do you need to install in order for your application to work?"
+        },
+        {
+            type: "input",
+            name: "use",
+            message: "How do you use this application?"
+        },
+        {
+            type: "input",
+            name: "languages",
+            message: "What programming languages did you use for this project?"
+        },
+        {
+            type: "list",
+            name: "license",
+            message: "What type of license would you like?",
+            choices: [
+                "Apache License 2.0",
+                "GNU GPLv3",
+                "MIT",
+                "ISC",
+                "None"
+            ]
+        },
+        {
+            type: "list",
+            name: "contributors",
+            message: "Where there any contributers on your project?",
+            choices: [
+                "Yes",
+                "No"
+            ]
+        }
+    ])
+    .then((response) => {
+        console.log(response)
+    })
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
 
-// Function call to initialize app
-init();
+// function call to initialize app
